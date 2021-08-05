f1def149d89558ede339b67e4154004b875ec356
Norudah <pierucci.romain@gmail.com>
2021-08-05 17:07:08 +0200

    Add feature : Computer can guess the number of the user

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import React, { useRef, useState } from "react";
+import { View, Text, Button, StyleSheet, Alert } from "react-native";
 import Card from "../components/Card";
 import NumberContainer from "../components/NumberContainer";
 import colors from "../constants/colors";
@@ -13,17 +13,55 @@ const generateRandomNumber = (min, max, exclude) => {
 };
 
 export default function GameScreen(props) {
-  const [computerNumber, setComputerNumber] = useState(generateRandomNumber(1, 99));
+  const [computerNumber, setComputerNumber] = useState(
+    generateRandomNumber(1, 100, props.guessNumber)
+  );
+
+  const lowerLimit = useRef(1);
+  const upperLimit = useRef(100);
+
+  const nextGuess = (action) => {
+    const hasLied =
+      (action === "lower" && props.guessNumber > computerNumber) ||
+      (action === "greater" && props.guessNumber < computerNumber) ||
+      props.guessNumber === computerNumber;
+
+    if (hasLied) {
+      Alert.alert(
+        "Eh oh !",
+        "Tu croyais que je n'allais pas m'en rendre compte ? On ne peut pas me mentir à moi !",
+        [{ text: "Désolé !", style: "destructive" }]
+      );
+      return;
+    }
+
+    if (action === "lower") upperLimit.current = computerNumber;
+    else lowerLimit.current = computerNumber;
+
+    const newComputerNumber = generateRandomNumber(
+      lowerLimit.current,
+      upperLimit.current
+    );
+    setComputerNumber(newComputerNumber);
+  };
 
   return (
     <View style={styles.screen}>
       <Text style={styles.screenTitle}>Laisse moi deviner...</Text>
+      <NumberContainer style={styles.numberContainer}>{computerNumber}</NumberContainer>
       <Card style={styles.card}>
         <Text style={styles.cardTitle}>Alors ? Je chauffe ou pas ?</Text>
-        <NumberContainer style={styles.numberContainer}>{computerNumber}</NumberContainer>
         <View style={styles.buttonsContainer}>
-          <Button title="C'est moins !" color={colors.primary} />
-          <Button title="C'est plus !" color={colors.secondary} />
+          <Button
+            title="C'est moins !"
+            color={colors.primary}
+            onPress={() => nextGuess("lower")}
+          />
+          <Button
+            title="C'est plus !"
+            color={colors.secondary}
+            onPress={() => nextGuess("greater")}
+          />
         </View>
       </Card>
     </View>
@@ -39,10 +77,11 @@ const styles = StyleSheet.create({
   screenTitle: {
     fontSize: 18,
     marginTop: 15,
-    marginBottom: 30,
   },
   card: {
     alignItems: "center",
+    justifyContent: "space-between",
+    minHeight: 90,
   },
   buttonsContainer: {
     flexDirection: "row",
@@ -50,8 +89,7 @@ const styles = StyleSheet.create({
     width: "100%",
   },
   numberContainer: {
-    marginBottom: 10,
-    marginTop: 20,
+    marginVertical: 20,
   },
   cardTitle: {
     fontSize: 16,
