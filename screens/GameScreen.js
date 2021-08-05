e570784adc2136fe5b688bbd1c91165cba91391e
Norudah <pierucci.romain@gmail.com>
2021-08-05 16:20:02 +0200

    Add feature : generate random integer in a specific range

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
 import Card from "../components/Card";
 import NumberContainer from "../components/NumberContainer";
 import colors from "../constants/colors";
 
+const generateRandomNumber = (min, max, exclude) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const number = Math.floor(Math.random() * (max - min) + min);
+  if (number === exclude) generateRandomNumber(min, max, exclude);
+  else return number;
+};
+
 export default function GameScreen(props) {
+  const [computerNumber, setComputerNumber] = useState(generateRandomNumber(1, 99));
+
   return (
     <View style={styles.screen}>
       <Text style={styles.screenTitle}>Laisse moi deviner...</Text>
       <Card style={styles.card}>
         <Text style={styles.cardTitle}>Alors ? Je chauffe ou pas ?</Text>
-        <NumberContainer style={styles.numberContainer}>XX</NumberContainer>
+        <NumberContainer style={styles.numberContainer}>{computerNumber}</NumberContainer>
         <View style={styles.buttonsContainer}>
           <Button title="C'est moins !" color={colors.primary} />
           <Button title="C'est plus !" color={colors.secondary} />
